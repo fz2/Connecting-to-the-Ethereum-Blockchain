b5be64954657c52e9f1bce97742a1ad8e8bc43a5
Fan Zhang <fanzhang@192.168.1.101>
2024-10-23 23:52:43 +0100

    first commit

--- reading_the_chain.py
+++ reading_the_chain.py
@@ -84,7 +84,7 @@ def get_contract_values(contract, admin_address, owner_address):
 	default_admin_role = int.to_bytes(0, 32, byteorder="big")
 
 	# TODO complete the following lines by performing contract calls
-	print(Contract.address)
+	print(contract.address)
 	print(contract.abi)
 	onchain_root = 0  # Get and return the merkleRoot from the provided contract
 	has_role = 0  # Check the contract to see if the address "admin_address" has the role "default_admin_role"
