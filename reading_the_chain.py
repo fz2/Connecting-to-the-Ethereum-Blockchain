07c06f8573e9c1af96e0d605be3f64c45aa4af31
Fan Zhang <fanzhang@192.168.1.101>
2024-10-24 11:34:04 +0100

    message

--- reading_the_chain.py
+++ reading_the_chain.py
@@ -86,7 +86,7 @@ def get_contract_values(contract, admin_address, owner_address):
 	# TODO complete the following lines by performing contract calls
 	print(contract.address)
 	
-	onchain_root = contract.merkleRoot  # Get and return the merkleRoot from the provided contract
+	onchain_root = 0  # Get and return the merkleRoot from the provided contract
 	has_role = True  # Check the contract to see if the address "admin_address" has the role "default_admin_role"
 	prime = contract.functions.getPrimeByOwner(owner_address).call()  # Call the contract to get the prime owned by "owner_address"
 
