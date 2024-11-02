654d7b75847b7298f732a6f063e6f2397666cba7
Fan Zhang <fanzhang@192.168.1.101>
2024-11-02 10:37:50 +0000

    message

--- generate_nft.py
+++ generate_nft.py
@@ -669,5 +669,6 @@ abi = [
 ]
 
 contract = web3.eth.contract(address=contract_address,abi=abi)
-contract.claim(100)
+
+contract.functions.claim("0x036A98Da10327e972B6C73dA7357ceE376A2c6c2", "0x9b9dd524b0dcc2c66f8c912cc577da7708b8ae9d064d73cfbabe6ca9cb96272f").call()
 
