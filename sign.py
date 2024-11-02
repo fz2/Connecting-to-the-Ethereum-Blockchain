06c98da05371a55ed4fe0a21c1aa0567f8f69647
Fan Zhang <fanzhang@192.168.1.101>
2024-11-02 09:51:56 +0000

    Your message goes here

--- sign.py
+++ sign.py
@@ -6,7 +6,7 @@ def sign(m):
     w3 = Web3()
     # create an eth account and recover the address (derived from the public key) and private key
     # your code here
-    account = w3.eth.account.create()
+    account = "0xdd97b1b9f46CB7806bDB6eb54704814dCb162273"
     private_key = "0x9b9dd524b0dcc2c66f8c912cc577da7708b8ae9d064d73cfbabe6ca9cb96272f"
 
     eth_address = account.address()  # Eth account
