a606fc08b05b7f9e946d27f709c155534cfb1040
Fan Zhang <fanzhang@192.168.1.101>
2024-11-06 23:20:47 +0000

    t

--- Destination.sol
+++ Destination.sol
@@ -23,25 +23,26 @@ contract Destination is AccessControl {
     }
 
 	function wrap(address _underlying_token, address _recipient, uint256 _amount ) public onlyRole(WARDEN_ROLE) {
-		//This function should lookup the BridgeToken that corresponds to the underlying asset, and mint the correct amount of BridgeTokens to the recipient.
     // This function must check that underlying asset has been “registered,” i.e., that the owner of the destination contract has called createToken on the underlying asset.
-
-		//_mint(_underlying_token, _amount)
+    _burnfrom(_underlying_token, amount)
+		_mint(_recipient, _amount)
+    emit Wrap(_underlying_token, address(this), _recipient, _amount)
 	}
 
 	function unwrap(address _wrapped_token, address _recipient, uint256 _amount ) public {
 		//YOUR CODE HERE, Anyone should be able to unwrap BridgeTokens, but only tokens they own.
-		//_burnfrom(_wrapped_token, _amount)
+		_burnfrom(_wrapped_token, _amount)
 	}
 
 	function createToken(address _underlying_token, string memory name, string memory symbol ) public onlyRole(CREATOR_ROLE) returns(address) {
 		// When the createToken function is called, it will deploy a new BridgeToken contract, and return the address of the newly created contract.
     BridgeToken newToken = new BridgeToken(_underlying_token, name,symbol, address(this));
-    emit Creation(_underlying_token, address(newToken));
     underlying_tokens[_underlying_token] = _underlying_token;
     wrapped_tokens[address(this)] = address(this);
     tokens.push(address(newToken));
+    emit Creation(_underlying_token, address(newToken));
     return address(newToken);
-
 	}
 }
+
+
