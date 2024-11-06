c37249be1909bea62a2c48b5af07a1a2764c3bf1
Fan Zhang <fanzhang@192.168.1.101>
2024-11-06 22:47:38 +0000

    t

--- Destination.sol
+++ Destination.sol
@@ -33,13 +33,12 @@ contract Destination is AccessControl {
 		//YOUR CODE HERE, Anyone should be able to unwrap BridgeTokens, but only tokens they own.
 		//_burnfrom(_wrapped_token, _amount)
 	}
+
 	function createToken(address _underlying_token, string memory name, string memory symbol ) public onlyRole(CREATOR_ROLE) returns(address) {
 		// When the createToken function is called, it will deploy a new BridgeToken contract, and return the address of the newly created contract.
-    require(msg.sender == CREATOR_ROLE);
-    BridgeToken newToken = BridgeToken(_underlying_token,name,symbol, CREATOR_ROLE);
-    emit Creation(_underlying_token, newToken.address);
-    return newToken.address;
+        BridgeToken newToken = new BridgeToken(_underlying_token, name,symbol, _underlying_token);
+        emit Creation(_underlying_token, address(newToken));
+        return address(newToken)
 
 	}
 }
-
