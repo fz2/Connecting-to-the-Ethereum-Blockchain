6093266dd1bf9dc92c2f01ae39842a1598fa1eed
fz2 <fz2@williams.edu>
2024-11-09 15:59:21 +0000

    Update Destination.sol

--- Destination.sol
+++ Destination.sol
@@ -24,7 +24,7 @@ contract Destination is AccessControl {
 
 	function wrap(address _underlying_token, address _recipient, uint256 _amount ) public onlyRole(WARDEN_ROLE) {
     // This function must check that underlying asset has been “registered,” i.e., that the owner of the destination contract has called createToken on the underlying asset.
-    require(wrapped_tokens[_underlying_token] != address[0]);
+    require(wrapped_tokens[_underlying_token] != address(0));
     BridgeToken wrapped_token = wrapped_tokens[_underlying_token];
 	  wrapped_token.mint(_recipient, _amount);
 
@@ -33,7 +33,7 @@ contract Destination is AccessControl {
 
 	function unwrap(address _wrapped_token, address _recipient, uint256 _amount ) public {
 		//YOUR CODE HERE, Anyone should be able to unwrap BridgeTokens, but only tokens they own.
-    require(underlying_tokens[_wrapped_token] != address[0]);
+    require(underlying_tokens[_wrapped_token] != address(0));
     BridgeToken (_wrapped_token).burnfrom(msg.sender, _amount);
     emit Unwrap(underlying_tokens[_wrapped_token],_wrapped_token, msg.sender, _recipient,_amount );
 	}
