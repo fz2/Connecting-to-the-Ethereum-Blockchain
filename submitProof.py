fe6cfb09c9f5da74f040bd062e947b3eb76c7a83
fz2 <fz2@williams.edu>
2024-11-11 01:05:29 +0000

    Update submitProof.py

--- submitProof.py
+++ submitProof.py
@@ -163,7 +163,7 @@ def send_signed_msg(proof, random_leaf):
     "nonce": w3.eth.get_transaction_count(acct_address),
 })
     signed_tx = w3.eth.account.sign_transaction(unsent_tx, private_key=private_key)
-    tx_hash = w3.eth.send_raw_transaction(signed_tx.raw_transaction)
+    tx_hash = w3.eth.send_raw_transaction(signed_tx.rawTransaction)
     
     return tx_hash
 
