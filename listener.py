98e5aff72801ec23f446bae624c2b9133eafe014
Fan Zhang <fanzhang@192.168.1.103>
2024-12-01 08:27:01 +0000

    new

--- listener.py
+++ listener.py
@@ -70,8 +70,11 @@ def scanBlocks(chain,start_block,end_block,contract_address):
                 }
             df.loc[count] = data
             count = count+1
-            df.to_csv(eventfile, mode= 'a', header= None, index=False)  
-         
+            df.to_csv(eventfile, mode= 'a', header= None, index=False) 
+        
+        final_df = pd.read_csv(eventfile)
+        final_df.columns = ['chain', 'token', 'recipient', 'amount', 'transactionHash', 'address']
+        final_df.to_csv(eventfile, index = False)         
     else:
         for block_num in range(start_block,end_block+1):
             event_filter = contract.events.Deposit.create_filter(fromBlock=block_num,toBlock=block_num,argument_filters=arg_filter)
@@ -90,5 +93,8 @@ def scanBlocks(chain,start_block,end_block,contract_address):
                 df.loc[count] = data
                 count = count+1
                 df.to_csv(eventfile, mode= 'a', header= None,index=False)  
+        final_df = pd.read_csv(eventfile)
+        final_df.columns = ['chain', 'token', 'recipient', 'amount', 'transactionHash', 'address']
+        final_df.to_csv(eventfile, index = False)     
 
 
